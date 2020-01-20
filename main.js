08482b51c29c209a8626226c29bf367f82685ba8
yuta-miyasaka38 <hlaupcpky38@gmail.com>
2020-01-21 08:37:01 +0900

    show page no: dpn() = display page number

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 (function(){
     'use strict';
-
-    /*var images =['images/pic01.jpg',
-                 'images/pic02.jpg',
-                 'images/pic03.jpg',
-                 'images/pic04.jpg',
-                 'images/pic05.jpg',
-                 'images/pic06.jpg'];*/
     
   var current = 0; //page番号取得用
   var mainImage = document.getElementById('mainImage');
@@ -16,12 +9,25 @@
     thumbs[i].addEventListener('click', function(){
       mainImage.src = 'images/' + this.dataset.image;// imgタグのsrc属性を書き換える
       current = this.dataset.index;
+      dpn();
     });
-  };
+  }
 
   var prev = document.getElementById('prev');
   var next = document.getElementById('next');
   var page = document.getElementById('page');
 
+    var images =['images/pic01.jpg',
+                 'images/pic02.jpg',
+                 'images/pic03.jpg',
+                 'images/pic04.jpg',
+                 'images/pic05.jpg',
+                 'images/pic06.jpg'];
+
+  function dpn(){
+    page.textContent = (Number(current)+1) + '/' + images.length;
+  }
+
+  dpn();
 
 })();
